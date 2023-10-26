066545a6f484e5125e6a3475b4957332aaa34315
unknown <atoygar16@ku.edu.tr>
2023-10-26 14:45:35 +0200

    Disable mentions if member already present in either channels

--- src/main.py
+++ src/main.py
@@ -39,8 +39,17 @@ async def on_voice_state_update(member: Member, before: VoiceState, after: Voice
         case (before_channel, after_channel):
             message = f'{member.display_name} switched from {before_channel.name} to {after_channel.name}'
 
+    present_members_in_channels = []
+
+    if after.channel:
+        present_members_in_channels.extend(list(map(lambda member: member.mention, after.channel.members)))
+    if before.channel:
+        present_members_in_channels.extend(list(map(lambda member: member.mention, before.channel.members)))
+
+    final_mention_list = list(filter(lambda mention: mention not in present_members_in_channels, config.get_mentions()))
+
     for message_channel in config.get_message_channels():
-        await message_channel.send(f'{list_to_string(config.get_mentions())} {message}')
+        await message_channel.send(f'{list_to_string(final_mention_list)} {message}')
 
 
 @client.tree.command()
@@ -71,7 +80,7 @@ async def unmention_me(interaction: Interaction):
 @app_commands.describe(
     arg='arg'
 )
-async def list(interaction: Interaction, arg: str):
+async def list_words(interaction: Interaction, arg: str):
     await interaction.response.send_message(arg)
 
 client.run(config.get_bot_token())
