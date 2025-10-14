dd9e862d03aa3122ac0a06561c0034de28ff6dc3
Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>
2025-10-14 19:57:45 +0200

    test: convert file for util tests into a class

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,20 +2,20 @@ from src.util import calculate_download_duration
 import pytest
 
 
-def test_calculate_download_duration():
-    assert calculate_download_duration("8", "8") == "137"
-    assert calculate_download_duration("8", "0") == "0"
+class TestCalculateDownloadDuration:
+    def test_returns_duration_for_valid_input(self):
+        assert calculate_download_duration("8", "8") == "137"
+        assert calculate_download_duration("8", "0") == "0"
 
+    def test_raises_exception_for_invalid_input(self):
+        with pytest.raises(ValueError):
+            calculate_download_duration("foo", "bar")
 
-def test_calculate_download_duration_raises_exception_if_invalid_input():
-    with pytest.raises(ValueError):
-        calculate_download_duration("foo", "bar")
+        with pytest.raises(ValueError):
+            calculate_download_duration("1", "bar")
 
-    with pytest.raises(ValueError):
-        calculate_download_duration("1", "bar")
+        with pytest.raises(ValueError):
+            calculate_download_duration("foo", "2")
 
-    with pytest.raises(ValueError):
-        calculate_download_duration("foo", "2")
-
-    with pytest.raises(ZeroDivisionError):
-        calculate_download_duration("0", "1")
+        with pytest.raises(ZeroDivisionError):
+            calculate_download_duration("0", "1")
